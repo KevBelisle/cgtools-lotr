19426d370834425a59b7e78fb4617707732689f2
Kevin-Belisle <kbelisle@addenergie.com>
2025-05-08 17:12:49 -0400

    Basic PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -12,6 +13,34 @@ export default defineConfig({
     }),
     react(),
     tsconfigPaths(),
+    VitePWA({
+      registerType: "prompt",
+      // workbox: {
+      //   globPatterns: ["**/*.{js,css,html,db,sqlite3}"],
+      // },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "LotR LCG - CardGame.tools",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   build: {
     target: "ES2022", // Enables top-level await
