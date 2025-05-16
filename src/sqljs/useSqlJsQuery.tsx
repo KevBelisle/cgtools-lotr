b2ce89d70f98098167b30f8364623d6064207a87
Kevin-Belisle <kbelisle@addenergie.com>
2025-05-16 16:59:32 -0400

    Rename type

--- src/sqljs/useSqlJsQuery.tsx
+++ src/sqljs/useSqlJsQuery.tsx
@@ -4,7 +4,7 @@ import { QueryExecResult } from "sql.js";
 import { SqljsDbContext } from "@/sqljs/SqljsProvider";
 
 const useSqljsQuery = (query: string) => {
-  const sqljsContext = useContext(SqljsDbContext);
+  const sqljsDbContext = useContext(SqljsDbContext);
 
   if (!SqljsDbContext) {
     throw new Error("useSqljsQuery must be used within a SqljsProvider");
@@ -14,13 +14,13 @@ const useSqljsQuery = (query: string) => {
   const [results, setResults] = useState<QueryExecResult[]>([]);
 
   useMemo(() => {
-    if (!sqljsContext.sqljsDb) {
+    if (!sqljsDbContext.sqljsDb) {
       return;
     }
 
     try {
       // const startTime = performance.now();
-      setResults(sqljsContext.sqljsDb.exec(query));
+      setResults(sqljsDbContext.sqljsDb.exec(query));
       // toaster.create({
       //   title: `Query executed in ${(performance.now() - startTime).toFixed(
       //     2
@@ -39,7 +39,7 @@ const useSqljsQuery = (query: string) => {
       }
       setResults([]);
     }
-  }, [sqljsContext.sqljsDb, query]);
+  }, [sqljsDbContext.sqljsDb, query]);
 
   return { error, results };
 };
