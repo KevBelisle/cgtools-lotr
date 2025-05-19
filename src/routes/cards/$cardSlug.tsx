f86dbb67b31584a41b1ec1253932e74a0782fd52
Kevin-Belisle <kbelisle@addenergie.com>
2025-05-18 23:35:48 -0400

    kysely query in tanstack router loader

--- src/routes/cards/$cardSlug.tsx
+++ src/routes/cards/$cardSlug.tsx
@@ -1,21 +1,33 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { kysely } from "@/sqljs/database-schema";
+import execCompiledQuery from "@/sqljs/exec-compiled-query";
+
 export const Route = createFileRoute("/cards/$cardSlug")({
   component: RouteComponent,
-  //   staleTime: 30 * 60 * 1000, // 30 minutes
-  //   preloadStaleTime: 30 * 60 * 1000, // 30 minutes
+
   loader: async ({ params, context }) => {
-    console.log({ params, context });
-    return context.sqljsDbContext.sqljsDb.exec("SELECT * FROM cards LIMIT 1");
+    const compiledQuery = kysely
+      .selectFrom("cards as c")
+      .leftJoin("cardSides as f", "f.Slug", "c.FrontSlug")
+      .leftJoin("cardSides as b", "b.Slug", "c.BackSlug")
+      .leftJoin("productCards as pc", "pc.CardSlug", "c.Slug")
+      .select([
+        "c.Slug",
+        "f.Title",
+        "f.Text",
+        "f.FlavorText",
+        "pc.FrontImageUrl",
+      ])
+      .where("c.Slug", "=", params.cardSlug)
+      .limit(1)
+      .compile();
+
+    return execCompiledQuery(compiledQuery, context.sqljsDbContext.sqljsDb)[0];
   },
 });
 
 function RouteComponent() {
-  const { cardSlug } = Route.useParams();
-  const posts = Route.useLoaderData();
-  return (
-    <div>
-      Hello "/cards/{cardSlug}"! {JSON.stringify(posts)}
-    </div>
-  );
+  const card = Route.useLoaderData();
+  return <div>{JSON.stringify(card, null, 2)}</div>;
 }
---
+++ src/sqljs/exec-compiled-query.tsx
@@ -0,0 +1,25 @@
+import { Database, SqlValue } from "sql.js";
+import { CompiledQuery } from "kysely";
+
+function execCompiledQuery<T>(
+  compiledQuery: CompiledQuery<T>,
+  db: Database
+): T[] {
+  const res = db.exec(
+    compiledQuery.sql,
+    compiledQuery.parameters as SqlValue[]
+  );
+
+  const cols = res[0].columns;
+  const ret = res[0].values.map((row) => {
+    const obj: Record<string, unknown> = {};
+    for (let i = 0; i < row.length; i++) {
+      obj[cols[i]] = row[i];
+    }
+    return obj;
+  });
+
+  return ret as T[];
+}
+
+export default execCompiledQuery;
