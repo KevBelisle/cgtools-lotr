a6a12f5e8f24832f73083a5e2e1c0ac4295d5818
Kevin-Belisle <kbelisle@addenergie.com>
2025-05-21 00:57:04 -0400

    Add vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     }),
     react(),
     tsconfigPaths(),
+    svgr(),
     VitePWA({
       registerType: "prompt", //"autoUpdate",
       workbox: {
