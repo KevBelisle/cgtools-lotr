cda648917a5c3161e74ab14dd3d522d0e8606692
Kevin-Belisle <kbelisle@addenergie.com>
2025-06-16 16:36:39 -0400

    Add indication of which cycles were repackaged

--- src/routes/products/original.tsx
+++ src/routes/products/original.tsx
@@ -1,7 +1,7 @@
 import { kysely } from "@/lotr/database-schema";
 import { ProductCard } from "@/lotr/display/product-card";
 import execCompiledQuery from "@/sqljs/exec-compiled-query";
-import { Box, Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/products/original")({
@@ -28,7 +28,12 @@ function RouteComponent() {
   const { products } = Route.useLoaderData();
 
   const coreProducts = products.filter((x) => x.Type === "Core");
-  const sagaProducts = products.filter((x) => x.Type === "Saga_Expansion");
+  const hobbitSagaProducts = products.filter(
+    (x) => x.Type === "Saga_Expansion" && x.Cycle === "The Hobbit",
+  );
+  const lotrSagaProducts = products.filter(
+    (x) => x.Type === "Saga_Expansion" && x.Cycle === "The Lord of the Rings",
+  );
   const cycles = Array.from(
     products
       .filter(
@@ -55,21 +60,28 @@ function RouteComponent() {
           Original Releases
         </Heading>
       </Box>
-      <Box as="main" display="flex" flexDirection="column" gap={4}>
+      <Box as="main" display="flex" flexDirection="column" gap={2}>
         <SimpleGrid gap={2} columns={{ base: 1, md: 2 }}>
           {coreProducts.map((product) => (
             <ProductCard key={product.Code} product={product} />
           ))}
         </SimpleGrid>
 
-        <Heading size="lg" textWrap="balance" mt={4}>
+        <Heading size="xl" textWrap="balance" mt={4}>
           Cycles
         </Heading>
         {cycles.map(([cycleName, cycleProducts]) => (
           <>
-            <Heading key={cycleName} size="sm" textWrap="balance" my={2}>
-              {cycleName}
+            <Heading key={cycleName} size="md" textWrap="balance" mt={2}>
+              {cycleName} cycle
             </Heading>
+            <Text mb="2" fontSize="sm" color="gray.500">
+              {["Angmar Awakened", "Dream-chaser", "Ered Mithrin"].some(
+                (x) => x === cycleName,
+              )
+                ? "This cycle has been repackaged as a Hero Expansion & Compaign Expansion."
+                : "This cycle has not been repackaged and is only available in its original release format."}
+            </Text>
             <SimpleGrid gap={2} columns={{ base: 1, md: 2 }}>
               {cycleProducts.map((product) => (
                 <ProductCard
@@ -87,10 +99,26 @@ function RouteComponent() {
         ))}
 
         <Heading size="lg" textWrap="balance" mt={4}>
-          The Hobbit & Lord of the Rings Sagas
+          The Hobbit saga expansions
         </Heading>
+        <Text mb="2" fontSize="sm" color="gray.500">
+          These saga expansions have not been repackaged and are only available
+          in their original release format.
+        </Text>
         <SimpleGrid gap={2} columns={{ base: 1, md: 2 }}>
-          {sagaProducts.map((product) => (
+          {hobbitSagaProducts.map((product) => (
+            <ProductCard key={product.Code} product={product} />
+          ))}
+        </SimpleGrid>
+
+        <Heading size="lg" textWrap="balance" mt={4}>
+          Lord of the Rings saga expansions
+        </Heading>
+        <Text mb="2" fontSize="sm" color="gray.500">
+          These saga expansions have been repackaged as 3 saga expansions.
+        </Text>
+        <SimpleGrid gap={2} columns={{ base: 1, md: 2 }}>
+          {lotrSagaProducts.map((product) => (
             <ProductCard key={product.Code} product={product} />
           ))}
         </SimpleGrid>
