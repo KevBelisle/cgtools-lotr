d1a65fb7603568b87b40e8e07a70ff613251043e
Kevin-Belisle <kbelisle@addenergie.com>
2025-06-19 00:12:48 -0400

    Fix small bug

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,7 @@ import { NotFound } from "@/components/pages/not-found";
 import { SearchFilterContextType } from "@/components/ui/advanced-filters-provider";
 import NavBar from "@/components/ui/nav-bar";
 import { SortOrderContextType } from "@/components/ui/sort-order-provider";
-import { RCOOnlyFilterContextType } from "@/lotr/rco-filter";
+import { RCOOnlyFilterContextType } from "@/lotr/rco-filter-provider";
 import { SqljsDbContextType } from "@/sqljs/sqljs-provider";
 
 interface RouterContext {
